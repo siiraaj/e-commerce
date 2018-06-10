c21baf6dd2cd923279750dfcc2f54e7c6b242692
siraj <siiraaj@yahoo.fr>
2018-06-10 20:40:26 +0200

    panier-livraison-validation

--- e-commerce/src/EcommerceBundle/Controller/panierController.php
+++ e-commerce/src/EcommerceBundle/Controller/panierController.php
@@ -10,4 +10,14 @@ class panierController extends Controller
     {
         return $this->render('EcommerceBundle:Default/panier/layout:panier.html.twig');
     }
+
+    public function livraisonAction()
+    {
+        return $this->render('EcommerceBundle:Default/panier/layout:livraison.html.twig');
+    }
+
+    public function validationAction()
+    {
+        return $this->render('EcommerceBundle:Default/panier/layout:validation.html.twig');
+    }
 }

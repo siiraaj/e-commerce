479d8a1924a7d8027418c916277a84027aee32bd
siraj <siiraaj@yahoo.fr>
2018-08-14 21:44:42 +0200

    insertion dans la base de donnees (test.html.twig) dump(produits)

--- e-commerce/src/EcommerceBundle/Controller/testController.php
+++ e-commerce/src/EcommerceBundle/Controller/testController.php
@@ -2,12 +2,18 @@
 
 namespace EcommerceBundle\Controller;
 
+use EcommerceBundle\Entity\Produits;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class testController extends Controller
 {
     public function ajouterAction()
     {
-        return $this->render('EcommerceBundle:Default/panier/layout:panier.html.twig');
+
+        $em = $this->getDoctrine()->getManager();
+        $produits =$em->getRepository('EcommerceBundle:Produits')->findAll();
+
+        return $this->render('EcommerceBundle:Default/produit/layout:test.html.twig',array('produits'=>$produits));
+
     }
 }
